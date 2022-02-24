d17c2236f7d79bc25a93f4a811740796573561ef
이성재 <xpsxm468@naver.com>
2022-02-25 00:35:05 +0900

    Add: 05. 기본 연산자

---
+++ lsj/05.js
@@ -0,0 +1,33 @@
+/*
+05. 기본 연산자
+
++, -, *, /, %, **
+
+우선 순위
+
+* /    >    + -
+
++=
+-=
+*=
+/=
+%=
+
+증가 연산자, 감소 연산자
+++
+--
+
+*/
+
+let num = 10;
+num += 5;
+console.log(num);  // 15
+
+let a = 0;
+let aa = a++;
+console.log(aa);  // 0
+
+let b = 0;
+let bb = ++b;
+console.log(bb);  // 1
+
